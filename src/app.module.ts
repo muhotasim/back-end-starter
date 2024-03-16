64c47b2f61d3f216841303c122fdb9520494f8cd
Muhotasim Fuad <muhotasimF@gmail.com>
2024-03-16 15:17:25 +0600

    updated

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,6 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { getDatabaseConfig } from './config/database';
 import { AuthModule } from './modules/auth/auth.module';
 import { CommonModule } from './modules/common/common.module';
-import { SeedCommand } from './commands/seed.command';
 
 @Module({
   imports: [
@@ -11,6 +10,6 @@ import { SeedCommand } from './commands/seed.command';
     AuthModule,
     CommonModule
   ],
-  providers: [SeedCommand]
+  providers: []
 })
 export class AppModule {}
